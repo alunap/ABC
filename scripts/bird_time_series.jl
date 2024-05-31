3ff2cac8f2e8f2d3e9a296cf9b5b72f98fb0d34d
Alun ap Rhisiart <coding@alunaprhisiart.uk>
2024-05-31 14:51:54 +0100

    Exclude CondaPkg files. Garvellach birds

--- scripts/bird_time_series.jl
+++ scripts/bird_time_series.jl
@@ -10,27 +10,44 @@ using DrWatson
 using DataFramesMeta, CSV, Dates
 using Parquet2: writefile
 
-birds = CSV.read(datadir("exp_raw", "ABC_2000_2022_dates.txt"), DataFrame; normalizenames=true, dateformat="dd/mm/yyyy", types=Dict(:Date => String, :Date_To => String), ignoreemptyrows=true, missingstring=["", "#N/A"])
+birds = CSV.read(datadir("exp_raw", "ABC_2000_2022.csv"), DataFrame;
+    normalizenames=true,
+    types=Dict(:Date => String, :Date_To => String),
+    ignoreemptyrows=true,
+    missingstring=["", "#N/A"])
 birds[!, Not([:Column14, :Column15])]
 
 function convertdate(str)
-    """The date may either be in dd/mm/yyyy or yyyy-mm-dd format"""
+    """The date may either be in dd/mm/yyyy or yyyy-mm-dd format.
+    First drop missing string, then deal with some known bad dates"""
     if ismissing(str)
         return ""
     end
-    if occursin("-", str)
+    d = Dict(
+        "10/10/10/2022" => "10/10/2022",
+        "2o/03/2022" => "20/03/2022",
+        "2403/2022" => "24/03/2022",
+        "2503/2022" => "25/03/2022",
+        "109/05/2022" => "19/05/2022",
+        "18.07.22" => "18/07/2022",
+        "[May 2021]" => "15/05/2021")
+    candidate = get(d, str, str) # convert known problems or return original value
+    if occursin("-", candidate)
         format = DateFormat("yyyy-mm-dd")
     else
         format = DateFormat("dd/mm/yyyy")
     end
-
-    returnDate(str, format)
+    try
+        date = Date(candidate, format)
+        return date
+    catch e
+        println("Bad date: $str")
+    end
 end
 
 @rtransform!(birds, :Date = convertdate(:Date), :Date_To = convertdate(:Date_To))
 
-writefile(datadir("exp_pro", "birds.parquet"), birds)
-
+CSV.write(datadir("exp_pro", "ABC_2000_2022.csv"), birds)
 # Generate species list for The Garvellachs
 dropmissing!(birds, [:Species, :Place])
 garvellachs = @chain birds begin
@@ -38,7 +55,7 @@ garvellachs = @chain birds begin
     unique(:Species)
     select([:Species, :Latin, :Place, :Gridref])
 end
-CSV.write("garvellach.csv", species_list)
+CSV.write(datadir("exp_pro", "garvellachs.csv"), garvellachs)
 
 dropmissing!(birds, [:Species, :Date, :Count])
 sort!(birds, :Date)
@@ -69,6 +86,10 @@ bird_counts = combine(birds_gdf, :Count => sum => :DayCount)
 # the next task in Python.
 writefile(datadir("exp_pro", "bird_counts.parquet"), bird_counts)
 writefile(datadir("exp_pro", "birds.parquet"), birds)
+
+using PythonCall
+
+
 mute_swan = @rsubset(bird_counts, :Species == "Mute Swan")
 
 # how many species are we dealing with?
